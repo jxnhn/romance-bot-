1a5c49f9e918c0355b71d7223bc67ca1019c0206
ye <jpedrosaraiva777@gmail.com>
2022-07-14 12:31:09 -0300

    "fixing a lot of bugs"

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import tweepy
 import pandas as pd
 
-<<<<<<< HEAD
 # authentication
 auth = tweepy.OAuth1UserHandler('api_key', 'api_key_secret')
 auth.set_access_token('access_token', 'access_token_secret')
-=======
+
 auth = tweepy.OAuth1UserHandler('my_keys',
                                 'keys',
                                 )
@@ -13,12 +12,6 @@ auth.set_access_token('keys',
                       'keys')
 api = tweepy.API(auth)
 
-
-# criando uma lista vazia de tweets
-
-def get_tweets(tweet):
-    tweets = []
->>>>>>> bb93fff8c47de64f6b57fba79bbf0b514a114f75
 
 api = tweepy.API(auth)
 
